288e81e1447e0fd0399fa88354579dbe13f2e109
Athou <jeremiepanzer@gmail.com>
2019-05-07 22:24:32 +0200

    scroll to entry when expanded

--- src/app/content/FeedEntry.tsx
+++ src/app/content/FeedEntry.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import Moment from 'react-moment';
 import { Card, Form } from 'semantic-ui-react';
 import { Clients } from '../..';
@@ -13,6 +13,7 @@ export const FeedEntry: React.FC<Props> = props => {
 
     const { dispatch } = useContext(AppContext)
     const [expanded, setExpanded] = useState(false)
+    const ref = useRef<HTMLDivElement>(null)
 
     function toggleExpanded() {
         setExpanded(!expanded)
@@ -29,34 +30,51 @@ export const FeedEntry: React.FC<Props> = props => {
         })).then(() => dispatch({ type: "entries.setRead", id: props.entry.id, feedId: +props.entry.feedId, read: !props.entry.read }))
     }
 
+    // scroll to entry when expanded
+    useEffect(() => {
+        if (!ref.current)
+            return
+
+        if (!expanded)
+            return
+
+        // TODO reusable constant somewhere
+        window.scrollTo({
+            top: ref.current.offsetTop - 37,
+            behavior: "smooth"
+        });
+    }, [expanded])
+
     return (
-        <Card.Group>
-            <Card fluid>
-                <Card.Content>
-                    <Card.Header onClick={() => toggleExpanded()} className="pointer">
-                        <img src={props.entry.iconUrl} alt="feed icon" style={{ width: "24px", height: "24px", marginRight: "5px" }} />
-                        <span style={{ fontWeight: props.entry.read ? "normal" : "bold" }}>
-                            {props.entry.title}
-                        </span>
-                    </Card.Header>
-                    <Card.Meta>
-                        from {props.entry.feedName} by {props.entry.author}, <Moment fromNow date={props.entry.date} />
-                    </Card.Meta>
-                    {expanded &&
-                        <Card.Description>
-                            <div>
-                                <div dangerouslySetInnerHTML={{ __html: props.entry.content }} />
-                                <Enclosure enclosureType={props.entry.enclosureType} enclosureUrl={props.entry.enclosureUrl} />
-                            </div>
-                        </Card.Description>
+        <div ref={ref}>
+            <Card.Group>
+                <Card fluid>
+                    <Card.Content>
+                        <Card.Header onClick={() => toggleExpanded()} className="pointer">
+                            <img src={props.entry.iconUrl} alt="feed icon" style={{ width: "24px", height: "24px", marginRight: "5px" }} />
+                            <span style={{ fontWeight: props.entry.read ? "normal" : "bold" }}>
+                                {props.entry.title}
+                            </span>
+                        </Card.Header>
+                        <Card.Meta>
+                            from {props.entry.feedName} by {props.entry.author}, <Moment fromNow date={props.entry.date} />
+                        </Card.Meta>
+                        {expanded &&
+                            <Card.Description>
+                                <div>
+                                    <div dangerouslySetInnerHTML={{ __html: props.entry.content }} />
+                                    <Enclosure enclosureType={props.entry.enclosureType} enclosureUrl={props.entry.enclosureUrl} />
+                                </div>
+                            </Card.Description>
+                        }
+                    </Card.Content>
+                    {expanded && <Card.Content>
+                        <Form.Checkbox label="Keep unread" onClick={() => toggleRead()} />
+                    </Card.Content>
                     }
-                </Card.Content>
-                {expanded && <Card.Content>
-                    <Form.Checkbox label="Keep unread" onClick={() => toggleRead()} />
-                </Card.Content>
-                }
-            </Card>
-        </Card.Group>
+                </Card>
+            </Card.Group>
+        </div>
     )
 }
 
