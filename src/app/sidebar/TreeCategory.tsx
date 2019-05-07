9abdc1f59d4672e5a1880e144523bf5e54db1efd
Jeremie Panzer <jeremie.panzer@erasme.ulb.ac.be>
2019-05-07 15:40:27 +0200

    display unread count next to categories and display bold label if they are collapsed

--- src/app/sidebar/TreeCategory.tsx
+++ src/app/sidebar/TreeCategory.tsx
@@ -1,9 +1,11 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useMemo, useState } from 'react';
 import { Icon, SemanticICONS } from 'semantic-ui-react';
 import { Clients } from '../..';
 import { Category, CollapseRequest } from '../../commafeed-api';
+import { flattenCategoryTree } from '../../utils';
 import { AppContext } from '../App';
 import { TreeNode } from './TreeNode';
+import { UnreadCount } from './UnreadCount';
 
 interface Props {
     category: Category,
@@ -14,6 +16,13 @@ export const TreeCategory: React.FC<Props> = props => {
 
     const [expanded, setExpanded] = useState(props.category.expanded)
     const { dispatch } = useContext(AppContext)
+
+
+    const unreadCount = useMemo(() => flattenCategoryTree(props.category)
+        .flatMap(c => c.feeds)
+        .map(f => f.unread)
+        .reduce((total, current) => total + current, 0)
+        , [props.category])
 
     function toggleExpanded() {
         Clients.category.collapse(new CollapseRequest({
@@ -26,9 +35,11 @@ export const TreeCategory: React.FC<Props> = props => {
         <div>
             <Icon name={props.icon ? props.icon : (expanded ? "chevron down" : "chevron right")}
                 onClick={() => toggleExpanded()} className="pointer" />
-            <span className="pointer" onClick={() => dispatch({ type: "navigateToCategory", categoryId: props.category.id })}>
+            <span className="pointer" style={{ fontWeight: (!expanded && unreadCount > 0) ? "bold" : "normal" }}
+                onClick={() => dispatch({ type: "navigateToCategory", categoryId: props.category.id })}>
                 {props.category.name}
             </span>
+            {!expanded && <UnreadCount unreadCount={unreadCount} />}
             {
                 expanded && (props.category.children.length > 0 || props.category.feeds.length > 0) &&
                 <div style={{ marginLeft: "20px" }}>
