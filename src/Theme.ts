ba5cd013eb1dc6f3cc518d20c159f69a0cb89b1b
Athou <jeremiepanzer@gmail.com>
2019-05-26 11:06:57 +0200

    use default dark theme

--- src/Theme.ts
+++ src/Theme.ts
@@ -4,14 +4,6 @@ export const DefaultTheme = createMuiTheme({})
 
 export const DarkTheme = createMuiTheme({
     palette: {
-        type: "dark",
-        text: {
-            primary: "#ebfff0",
-            secondary: "#8e9297"
-        },
-        background: {
-            default: "#36393f",
-            paper: "#2f3136"
-        }
+        type: "dark"
     }
 })
