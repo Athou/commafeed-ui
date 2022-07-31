3f36f5ed710026753de2f17ba60a7d9a70f5c8d8
Athou <jeremiepanzer@gmail.com>
2022-07-31 10:31:04 +0200

    move button to reusable component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,38 +1,13 @@
-import { ActionIcon, Button, Center, createStyles, Divider, Group, Text, useMantineTheme } from "@mantine/core"
-import { useMediaQuery } from "@mantine/hooks"
+import { Center, createStyles, Divider, Group, Text } from "@mantine/core"
 import { openConfirmModal } from "@mantine/modals"
 import { markAllEntries, reloadEntries } from "app/slices/entries"
 import { changeReadingMode, changeReadingOrder, reloadProfile, reloadSettings } from "app/slices/user"
 import { useAppDispatch, useAppSelector } from "app/store"
+import { ActionButton } from "components/ActionButtton"
 import { Loader } from "components/Loader"
-import { forwardRef, useEffect } from "react"
+import { useEffect } from "react"
 import { FaArrowDown, FaArrowUp, FaCheckDouble, FaEye, FaEyeSlash, FaSyncAlt, FaUser } from "react-icons/fa"
 import { ProfileMenu } from "./ProfileMenu"
-
-interface HeaderButtonProps {
-    className?: string
-    icon?: React.ReactNode
-    label?: string
-    onClick?: React.MouseEventHandler
-}
-
-/**
- * Switches between Button with label (desktop) and ActionIcon (mobile)
- */
-const HeaderButton = forwardRef<HTMLButtonElement, HeaderButtonProps>((props: HeaderButtonProps, ref) => {
-    const theme = useMantineTheme()
-    const mobile = !useMediaQuery(`(min-width: ${theme.breakpoints.lg}px)`)
-    return mobile ? (
-        <ActionIcon ref={ref} color={theme.primaryColor} variant="subtle" className={props.className} onClick={props.onClick}>
-            {props.icon}
-        </ActionIcon>
-    ) : (
-        <Button ref={ref} variant="subtle" size="xs" className={props.className} leftIcon={props.icon} onClick={props.onClick}>
-            {props.label}
-        </Button>
-    )
-})
-HeaderButton.displayName = "HeaderButton"
 
 const HeaderDivider = () => <Divider sx={{ height: "28px" }} orientation="vertical" />
 
@@ -80,24 +55,24 @@ export const Header = () => {
     return (
         <Center>
             <Group>
-                <HeaderButton icon={<FaSyncAlt />} label="Refresh" onClick={() => dispatch(reloadEntries())}></HeaderButton>
+                <ActionButton icon={<FaSyncAlt />} label="Refresh" onClick={() => dispatch(reloadEntries())}></ActionButton>
                 <HeaderDivider />
-                <HeaderButton icon={<FaCheckDouble />} label="Mark all as read" onClick={openMarkAllEntriesModal}></HeaderButton>
+                <ActionButton icon={<FaCheckDouble />} label="Mark all as read" onClick={openMarkAllEntriesModal}></ActionButton>
                 <HeaderDivider />
-                <HeaderButton
+                <ActionButton
                     className={classes.button}
                     icon={settings.readingMode === "all" ? <FaEye /> : <FaEyeSlash />}
                     label={settings.readingMode}
                     onClick={() => dispatch(changeReadingMode(settings.readingMode === "all" ? "unread" : "all"))}
-                ></HeaderButton>
-                <HeaderButton
+                ></ActionButton>
+                <ActionButton
                     className={classes.button}
                     icon={settings.readingOrder === "asc" ? <FaArrowUp /> : <FaArrowDown />}
                     label={settings.readingOrder}
                     onClick={() => dispatch(changeReadingOrder(settings.readingOrder === "asc" ? "desc" : "asc"))}
-                ></HeaderButton>
+                ></ActionButton>
                 <HeaderDivider />
-                <ProfileMenu control={<HeaderButton icon={<FaUser />} label={profile?.name} />} />
+                <ProfileMenu control={<ActionButton icon={<FaUser />} label={profile?.name} />} />
             </Group>
         </Center>
     )
