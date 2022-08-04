d5c97da1e3b5dd27675792ed84c1bdd43d4ea97b
Athou <jeremiepanzer@gmail.com>
2022-08-04 14:39:35 +0200

    RelativeDate now updates itself every minute

--- src/components/RelativeDate.tsx
+++ src/components/RelativeDate.tsx
@@ -1,6 +1,11 @@
 import dayjs from "dayjs"
+import { useState } from "react"
+import { useInterval } from "react-use"
 
 export function RelativeDate(props: { date: Date | number }) {
+    const [now, setNow] = useState(new Date())
+    useInterval(() => setNow(new Date()), 60 * 1000)
+
     if (!props.date) return <>"N/A"</>
-    return <>{dayjs(props.date).fromNow()}</>
+    return <>{dayjs(props.date).from(dayjs(now))}</>
 }
