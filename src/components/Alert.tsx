8f40c82468ad5f3fcad1ec45855788f567f14d68
Athou <jeremiepanzer@gmail.com>
2022-08-05 15:44:16 +0200

    add default icon for alerts

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,18 +1,21 @@
 import { t } from "@lingui/macro"
 import { Alert as MantineAlert, Box, MantineColor } from "@mantine/core"
-import { Fragment } from "react"
+import React, { Fragment } from "react"
+import { TbCircleX } from "react-icons/tb"
 
 export interface ErrorsAlertProps {
     title?: string
     color?: MantineColor
+    icon?: React.ReactNode
     messages: string[]
 }
 
 export function Alert(props: ErrorsAlertProps) {
     const title = props.title ?? t`Error!`
     const color = props.color ?? "red"
+    const icon = props.icon ?? <TbCircleX />
     return (
-        <MantineAlert title={title} color={color}>
+        <MantineAlert title={title} color={color} icon={icon}>
             {props.messages.map((m, i) => (
                 <Fragment key={m}>
                     <Box>{m}</Box>
